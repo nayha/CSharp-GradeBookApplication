caf9b5892e1731b52a7c7f102a60c50525239af0
nayJlt <htetaung_nay@jltasia.com>
2018-03-01 15:52:18 +0800

    7.5

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -109,20 +109,31 @@ namespace GradeBook.GradeBooks
             }
         }
 
-        public virtual double GetGPA(char letterGrade, StudentType studentType)
+        public virtual int GetGPA(char letterGrade, StudentType studentType)
         {
+            var gpa = 0;
+
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    gpa = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    gpa = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    gpa = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    gpa = 1;
+                    break;
             }
-            return 0;
+
+            if (IsWeighted == true && (studentType.ToString() == "Honors" || studentType.ToString() == "DualEnrolled")) {
+                gpa++;
+            }
+
+            return gpa;
         }
 
         public virtual void CalculateStatistics()

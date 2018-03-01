f4a3e6e2955b6319fa221122c0e2f256c8b34fae
nayJlt <htetaung_nay@jltasia.com>
2018-03-01 15:02:28 +0800

    3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,8 +6,44 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(String name) : base(name) {
+        public RankedGradeBook(String name) : base(name)
+        {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            }
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.20);
+            List<double> grades = new List<double>();
+            foreach (var student in Students)
+            {
+                grades.Add(student.AverageGrade);
+            }
+            grades.Sort((a, b) => -1 * a.CompareTo(b));
+
+            if (grades[threshold - 1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+
+            return 'F';
         }
     }
 }

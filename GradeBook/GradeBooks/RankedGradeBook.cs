8a07228f502bf301997fba6b7145f010e2047987
nayJlt <htetaung_nay@jltasia.com>
2018-03-01 15:13:04 +0800

    4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -45,5 +45,23 @@ namespace GradeBook.GradeBooks
 
             return 'F';
         }
+
+        public override void CalculateStatistics() {
+            if (Students.Count < 5)
+            {
+                Console.Write("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(String name) {
+            if (Students.Count < 5)
+            {
+                Console.Write("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
